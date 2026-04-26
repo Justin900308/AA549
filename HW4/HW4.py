f90c7ad68b4efca4530d4e51cc49613fb38d711e
justin <wow3987@gmail.com>
2026-04-25 19:50:03 -0700

    Update

---
+++ HW4/HW4.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cvxpy as cp
+import csv
+import scipy as sy
+from matplotlib.ticker import FormatStrFormatter
+import numpy.random as rnd
+
