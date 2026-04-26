1599e2a69a5b93eb269e77186f25b307b6218949
justin900308 <wow3987@gmail.com>
2026-04-26 13:32:51 -0700

    update

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -5,6 +5,7 @@ import csv
 import scipy as sy
 from matplotlib.ticker import FormatStrFormatter
 import numpy.random as rnd
+import scipy.linalg as la
 
 ## define global constants
 N = 1000
@@ -28,19 +29,46 @@ def gen_Y_X1(X0, U, W):
     return Y, X1
 
 
-def F_lin(Y, P, R):
+def F_lin(Y, X1, P, R, methods):
     X_est = (P + 1) / (P + R + 1) * Y
     return X_est
 
 
-def scatter_plot(N, P, R, estimator, est_name):
+def compute_E_and_Cov(X1, phi):
+    N = len(X1)
+    Exp_phi = np.sum(phi) / N  ## this is a vector of length 2
+    Exp_X1 = np.sum(X1) / N  ## this is a scalar
+    Cov_phi = np.sum((phi - Exp_phi) @ (phi - Exp_phi).T) / N  ## this is N x N matrix
+    Cov_X_phi = np.sum((X1 - Exp_X1) @ (phi - Exp_phi).T) / N  ## this is N dim vector
+    return Exp_X1, Exp_phi, Cov_phi, Cov_X_phi
+
+
+def compute_phi(Y, methods):
+    if methods == "case 1":
+        first = Y
+        second = np.multiply(np.multiply(Y, Y), Y)
+    else:
+        first = Y
+        second = np.sign(Y)
+    phi = np.vstack((first, second))
+    return phi
+
+
+def F_nolinear(Y, X1, P, R, methods):
+    phi = compute_phi(Y, methods)
+    Exp_X1, Exp_phi, Cov_phi, Cov_X_phi = compute_E_and_Cov(X1, phi)
+    X_est = Exp_X1 + Cov_X_phi @ la.inv(Cov_phi) @ (phi - Exp_phi)
+    return X_est
+
+
+def scatter_plot(N, P, R, estimator, methods):
     U = gen_U(N)
     #### plot  the scatter plots
     Y, X1 = gen_Y_X1(X0, U, W)
     fig, ax = plt.subplots()
     if estimator is not None:
-        X_est = estimator(Y, P, R)
-        ax.scatter(X_est, Y, label=f"{est_name}, P = {P}, R = {R} ")
+        X_est = estimator(Y, X1, P, R, methods)
+        ax.scatter(X_est, Y, label=f"{methods}, P = {P}, R = {R} ")
         ax.legend()
         ax.set(xlabel='X est', ylabel='Y')
     else:
@@ -52,7 +80,11 @@ def scatter_plot(N, P, R, estimator, est_name):
     return
 
 
-## no estimation
-scatter_plot(N, P, R, None, None)
-## linear estimation
-scatter_plot(N, P, R, F_lin, "Linear")
+# ## no estimation
+# scatter_plot(N, P, R, None, None)
+# ## linear estimation
+# scatter_plot(N, P, R, F_lin, "Linear")
+## nonlinear estimation 1
+scatter_plot(N, P, R, F_nolinear, "case 1")
+## nonlinear estimation 2
+scatter_plot(N, P, R, F_nolinear, "case 2")
